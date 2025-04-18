9b6bd3a712c0d17b92b2c34637e67fd858f731e6
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-04-18 18:06:07 -0400

    FIXED instead of a struct LZWEncoder is now a mod (there was no need for a struct) and instead of using string we are using vectors of bytes because strings are turbo cringe 🤮

--- src/lzw_encoder.rs
+++ src/lzw_encoder.rs
@@ -1,29 +1,17 @@
-use std::collections::{HashMap, HashSet};
+pub mod LZWEncoder {
+    use std::collections::HashMap;
 
-use crate::varsize::{decode_varsize, encode_varsize};
+    use crate::varsize::{decode_varsize, encode_varsize};
 
-// NOTE possibly remove struct and just use the methods
-
-pub struct LZWEncoder {
-    dict: HashMap<String, usize>,
-    pub encoding: Vec<String>,
-    pub data: Vec<usize>,
-}
-
-impl LZWEncoder {
-    pub fn new() -> Self {
-        LZWEncoder {
-            dict: HashMap::new(),
-            encoding: Vec::new(),
-            data: Vec::new(),
-        }
-    }
-
-    fn insert(&mut self, codeword: String) {
-        let index = self.encoding.len();
-        self.encoding.push(codeword.clone());
-
-        self.dict.insert(codeword, index);
+    fn insert(
+        // &self,
+        codeword: Vec<u8>,
+        codewords: &mut HashMap<Vec<u8>, usize>,
+        encoding: &mut Vec<Vec<u8>>,
+    ) {
+        let index = encoding.len();
+        encoding.push(codeword.clone());
+        codewords.insert(codeword, index);
     }
 
     fn usize_to_bytes(xs: &[usize]) -> Vec<u8> {
@@ -37,41 +25,52 @@ impl LZWEncoder {
         bytes
     }
 
-    pub fn encode(&mut self, stream: Vec<char>) -> Vec<u8> {
+    /// Encode given bytes using the lzw encoding
+    ///
+    /// @**returns** (Vec\<u8\>, Vec\<u8\>) => (single chars in order, the encoded indexes)
+    pub fn encode(input: &[u8]) -> (Vec<u8>, Vec<u8>) {
+        let mut indexes: Vec<usize> = Vec::new();
+        let mut unique_char: Vec<u8> = Vec::new();
+        let mut codewords: HashMap<Vec<u8>, usize> = HashMap::new();
+
+        let mut encoding: Vec<Vec<u8>> = Vec::new();
         // add unique char to dict
-        for c in &stream {
-            let codeword = c.to_string();
-            if let None = self.dict.get(&codeword) {
-                self.insert(codeword);
+        for &c in input {
+            let codeword = vec![c];
+
+            if let None = codewords.get(&codeword) {
+                unique_char.push(c);
+                insert(codeword, &mut codewords, &mut encoding);
             };
         }
 
         // Encoding the rest while creating new codewords
         let mut i: usize = 0;
-        while i < stream.len() {
-            let mut current = stream[i].to_string();
+        while i < input.len() {
+            let mut current = vec![input[i]];
             let mut next = i + 1;
             let mut index = 0;
 
-            while let Some(codeword_index) = self.dict.get(&current) {
+            while let Some(codeword_index) = codewords.get(&current) {
                 index = *codeword_index;
-                if next == stream.len() {
+
+                if next == input.len() {
                     // adding char that will be removed afterward
-                    current.push('\0');
+                    current.push(0);
                     break;
                 }
-                current.push(stream[next]);
+                current.push(input[next]);
                 next += 1;
             }
-
-            let done = current.chars().last().unwrap() == '\0';
+            // NOTE validation can be better
+            let done = current[current.len() - 1] == 0;
             if !done {
                 let new_codeword = current.clone();
-                self.insert(new_codeword);
+                insert(new_codeword, &mut codewords, &mut encoding);
                 current.pop();
             }
 
-            self.data.push(index);
+            indexes.push(index);
 
             i = next - 1;
             if done {
@@ -83,104 +82,82 @@ impl LZWEncoder {
         // println!("dict: {:?}", self.dict);
         // println!("encoding: {:?}", self.encoding);
         // println!("data: {:?}", self.data);
-        LZWEncoder::usize_to_bytes(&self.data)
+        // LZWEncoder::usize_to_bytes(&self.data)
+        (unique_char, usize_to_bytes(&indexes))
     }
 
     /// Decode previously encoded data
     /// -
-    pub fn decode(&mut self, single_chars: Vec<char>, stream: &[u8]) -> String {
-        // pub fn decode(&mut self, single_chars: Vec<char>, stream: &[usize]) -> String {
-        let stream = decode_varsize(stream);
+    pub fn decode(single_chars: Vec<u8>, input: &[u8]) -> Vec<u8> {
+        let mut codewords: HashMap<Vec<u8>, usize> = HashMap::new();
+
+        let mut encoding: Vec<Vec<u8>> = Vec::new();
+        let input = decode_varsize(input);
 
         // add unique char to dict
-        for c in &single_chars {
-            let codeword = c.to_string();
-            self.insert(codeword);
+        for c in single_chars {
+            let codeword = vec![c];
+            insert(codeword, &mut codewords, &mut encoding);
         }
 
-        let mut decoded = Vec::new();
+        let mut decoded_chunks = Vec::new();
 
-        let mut previous_string = String::from("");
+        let mut previous_string = Vec::new();
         let mut first = true;
-        for index in stream {
-            let word = if (index as usize) < self.encoding.len() {
-                self.encoding[index as usize].clone()
-            } else if (index as usize) == self.encoding.len() {
-                format!(
-                    "{}{}",
-                    previous_string,
-                    previous_string.chars().next().unwrap()
-                )
+        for index in input {
+            let word = if (index as usize) < encoding.len() {
+                encoding[index as usize].clone()
+            } else if (index as usize) == encoding.len() {
+                let mut new_word = previous_string.clone();
+                new_word.push(previous_string[0]);
+                new_word
             } else {
                 panic!("Should no be possible");
             };
 
             // DEBUG
             // println!("word: {}, index: {}", word.clone(), index);
-            decoded.push(word.clone());
+
+            decoded_chunks.push(word.clone());
 
             if !first {
-                self.encoding.push(format!(
-                    "{}{}",
-                    previous_string,
-                    word.chars().next().unwrap()
-                ));
+                let mut new_encoding = previous_string.clone();
+                new_encoding.push(word[0]);
+                encoding.push(new_encoding);
             }
 
             first = false;
             previous_string = word;
-
-            // DEBUG
-            // println!("encoding: {:?}", self.encoding.join(","));
-            // println!("decoded: {:?}", decoded.join(""));
-            // println!();
         }
 
-        self.encoding.pop();
-        let decoded = decoded.join("");
+        // NOTE useless? Will let it there in case we need to return the encoding at some point.
+        encoding.pop();
 
-        // DEBUG
-        // println!("encoding: {:?}", self.encoding.join(","));
-        // println!("decoded: {:?}", decoded.join(""));
-        // println!();
+        let mut decoded = Vec::new();
+
+        for decoded_chunk in decoded_chunks {
+            decoded.extend_from_slice(&decoded_chunk);
+        }
 
         decoded
-    }
-
-    pub fn get_unique_sequent_char(input: &str) -> Vec<char> {
-        let mut discovered_chars: HashSet<char> = HashSet::new();
-        let mut chars = Vec::new();
-        for c in input.chars() {
-            if let None = discovered_chars.get(&c) {
-                chars.push(c);
-                discovered_chars.insert(c);
-            }
-        }
-
-        chars
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{
-        compressed_buffer::{self, Bit, CompressedBuffer},
-        huffman_tree::HuffmanTree,
+        compressed_buffer::CompressedBuffer, huffman_tree::HuffmanTree,
+        utils::display_data_compression_ratio,
     };
 
     use super::*;
 
     #[test]
-    fn test_encode_variable_width_code() {}
-
-    #[test]
     fn test_encode() {
-        let to_encode = vec!['A', 'A', 'B', 'A', 'B', 'C', 'C', 'A', 'B', 'C'];
+        let text = "AABABCCABC";
+        let to_encode: Vec<u8> = text.bytes().collect();
 
-        let mut encoder = LZWEncoder::new();
-
-        let encoded = encoder.encode(to_encode);
-        println!("encoded: {:?}", encoded);
+        let (_, encoded) = LZWEncoder::encode(&to_encode);
 
         assert_eq!(vec![0, 0, 1, 4, 2, 2, 6], encoded);
     }
@@ -189,23 +166,20 @@ mod tests {
     fn test_decode() {
         let to_decode = [0, 0, 1, 4, 2, 2, 6];
 
-        let chars = vec!['A', 'B', 'C'];
-        let mut encoder = LZWEncoder::new();
+        let single_chars = vec![65u8, 66u8, 67u8];
+        let decoded = LZWEncoder::decode(single_chars, &to_decode);
 
-        let decoded = encoder.decode(chars, &to_decode);
-
-        assert_eq!("AABABCCABC", decoded);
-        // encoder.encode(&to_decode);
+        let text: Vec<u8> = "AABABCCABC".bytes().collect();
+        assert_eq!(text, decoded);
     }
 
     #[test]
     fn encode_n_decode() {
         let text = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffff";
         // let text = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffffsdashjdgasjhdgasjhdbvasjhvdjhasgdajhsgdkhasgdjgvbgwsyfghewirfuywgyubefkhicruygwesyurfhgb uyeg rbwnhs jbgvfzfgujwa jge He";
-        let to_encode: Vec<char> = text.chars().collect();
+        let to_encode: Vec<u8> = text.bytes().collect();
 
-        let mut encoder = LZWEncoder::new();
-        let to_decode = encoder.encode(to_encode);
+        let (single_chars, to_decode) = LZWEncoder::encode(&to_encode);
 
         // DEBUG
         // println!("Initial length: {}", text.len());
@@ -213,46 +187,39 @@ mod tests {
         // println!("encoded length: {}", to_decode.len() * 8);
         // println!("encoded: {:?}", to_decode);
 
-        let chars = LZWEncoder::get_unique_sequent_char(text);
-        let mut encoder = LZWEncoder::new();
-        let decoded = encoder.decode(chars, &to_decode);
+        let decoded = LZWEncoder::decode(single_chars, &to_decode);
 
-        assert_eq!(text, decoded);
+        assert_eq!(to_encode, decoded);
     }
 
     #[test]
     fn encode_n_decode_default_text_255() {
-        let to_encode: Vec<char> = (1..=255).map(|c: u8| c as char).collect();
+        let to_encode: Vec<u8> = (1..=255).collect();
 
         // let mut to_encode2: Vec<char> = (1..=255).map(|c: u8| c as char).collect();
         // to_encode.append(&mut to_encode2);
 
-        let mut encoder = LZWEncoder::new();
-        let to_decode = encoder.encode(to_encode.clone());
+        let (single_chars, to_decode) = LZWEncoder::encode(&to_encode);
 
-        let text: String = to_encode.iter().collect();
-
-        let chars = LZWEncoder::get_unique_sequent_char(&text);
-        let mut encoder = LZWEncoder::new();
-        let decoded = encoder.decode(chars, &to_decode);
+        let decoded = LZWEncoder::decode(single_chars, &to_decode);
 
         // DEBUG
         // println!("decoded.len(): {}", decoded.len());
         // println!("text: {}", text.len());
 
-        assert_eq!(text, decoded);
+        assert_eq!(to_encode, decoded);
     }
 
     // NOTE to run the following to get the compression rates:
     // cargo test parsing_encoding -- --nocapture
     #[test]
     fn parsing_encoding_through_huffman_encoding() {
-        let text = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffffsdashjdgasjhdgasjhdbvasjhvdjhasgdajhsgdkhasgdjgvbgwsyfghewirfuywgyubefkhicruygwesyurfhgb uyeg rbwnhs jbgvfzfgujwa jge He";
+        let text = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffff";
+        // let text = "aaaaabbbbbbbbbccccccccccccdddddddddddddeeeeeeeeeeeeeeeefffffffffffffffffffffffffffffffffffffffffffffsdashjdgasjhdgasjhdbvasjhvdjhasgdajhsgdkhasgdjgvbgwsyfghewirfuywgyubefkhicruygwesyurfhgb uyeg rbwnhs jbgvfzfgujwa jge He";
 
         // LZW ENCODING
-        let to_encode: Vec<char> = text.chars().collect();
-        let mut encoder = LZWEncoder::new();
-        let lzw_encoded = encoder.encode(to_encode);
+        let to_encode: Vec<u8> = text.bytes().collect();
+        let (_, lzw_encoded) = LZWEncoder::encode(&to_encode);
 
         // HUFFMAN_ENCODING
         let to_encode_with_huffman: Vec<u8> = text.bytes().collect();
@@ -263,9 +230,8 @@ mod tests {
             .for_each(|&bit| compressed_buffer_huffman.push_bit(bit));
 
         // LZW + HUFFMAN encoding
-        let to_encode: Vec<char> = text.chars().collect();
-        let mut lzw_encoder = LZWEncoder::new();
-        let encoded_with_lzw = lzw_encoder.encode(to_encode);
+        let to_encode: Vec<u8> = text.bytes().collect();
+        let (single_chars_huff_lzw, encoded_with_lzw) = LZWEncoder::encode(&to_encode);
 
         let tree = HuffmanTree::load_tree_from_bytes(&encoded_with_lzw);
         let mut compressed_buffer = CompressedBuffer::new();
@@ -273,32 +239,23 @@ mod tests {
         bits.iter().for_each(|&bit| compressed_buffer.push_bit(bit));
 
         // DEBUG print compression rates
-        //
-        println!("Initial length: {}", text.len());
-        println!(
-            "encoded length (with lzw only): {}. Total compression rate: {:.2} %",
-            lzw_encoded.len(),
-            (lzw_encoded.len() as f64 / text.len() as f64) * 100.0
-        );
-        println!(
-            "encoded length (huffman only): {}. Total compression rate: {:.2} %",
-            compressed_buffer_huffman.buffer.len(),
-            (compressed_buffer_huffman.buffer.len() as f64 / text.len() as f64) * 100.0
-        );
-        println!(
-            "encoded length (with lzm followed by huffman): {}. Total compression rate: {:.2} %",
-            compressed_buffer.buffer.len(),
-            (compressed_buffer.buffer.len() as f64 / text.len() as f64) * 100.0
-        );
+        println!("LZW ONLY:");
+        display_data_compression_ratio(text.len(), lzw_encoded.len());
+
+        println!("HUFFMAN ONLY:");
+        display_data_compression_ratio(text.len(), compressed_buffer_huffman.buffer.len());
+
+        println!("LZM FOLLOWED BY HUFFMAN:");
+        display_data_compression_ratio(text.len(), compressed_buffer.buffer.len());
+        // END DEBUG //
 
         let decoded_huffman = tree.decode(&compressed_buffer.buffer, bits.len());
 
         assert_eq!(encoded_with_lzw, decoded_huffman);
 
-        let mut lzw_decoder = LZWEncoder::new();
-        let single_chars = LZWEncoder::get_unique_sequent_char(text);
-        let decoded = lzw_decoder.decode(single_chars, &decoded_huffman);
+        let decoded = LZWEncoder::decode(single_chars_huff_lzw, &decoded_huffman);
 
+        let text: Vec<u8> = text.bytes().collect();
         assert_eq!(text, decoded);
     }
 }
