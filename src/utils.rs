5159960d168ce844c57cd18cb7ad7c82c19939f8
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-04-23 17:02:35 -0400

    added a way to verify the entropy of a given set of values

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+#[allow(dead_code)]
 /// display the data compression ratio
 ///
 /// based on the following: <https://en.wikipedia.org/wiki/Data_compression_ratio>
@@ -13,4 +16,46 @@ pub fn display_data_compression_ratio(
     println!("\t Compressed size: {compressed_size} bytes");
     println!("\t compression ratio: {compression_ratio:.1}:1");
     println!("\t space saving: {} %", space_saving * 100.);
+}
+
+#[allow(dead_code)]
+/// get the entropy for given values
+pub fn entropy(values: &[u8]) -> f64 {
+    let mut freq: HashMap<u8, usize> = HashMap::new();
+    let mut total: usize = 0;
+
+    for key in values {
+        total += 1;
+        if let Some(val) = freq.get_mut(key) {
+            *val += 1;
+        } else {
+            freq.insert(*key, 1);
+        }
+    }
+
+    let ps: Vec<f64> = freq
+        .iter()
+        .map(|(_, &num)| num as f64 / total as f64)
+        .collect();
+
+    let mut entropy: f64 = 0.;
+    for p in ps {
+        entropy += p * p.log2();
+    }
+
+    -entropy
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_entropy() {
+        let bytes: Vec<u8> = "AABBCCDD".bytes().collect();
+        let entropy = entropy(&bytes);
+
+        assert_eq!(2., entropy);
+    }
 }
