3185a3d8c2e9f9b6d135b6f4b34ecf674e29b96b
Lajoie, Samuel <lajoie.samuel@courrier.uqam.ca>
2025-04-23 18:27:07 -0400

    added parallel burrows wheeler transform encoding

--- src/burrows_wheeler.rs
+++ src/burrows_wheeler.rs
@@ -31,6 +31,50 @@ pub mod BurrowsWheeler {
         (index, transformed)
     }
 
+    // TODO move to front algo
+
+    use std::sync::{Arc, Mutex};
+
+    use rayon::prelude::*;
+
+    /// same functionality but works in parallel
+    pub fn encode_par(input: &[u8]) -> (usize, Vec<u8>) {
+        let lenght = input.len();
+
+        // per-row mutexes to reduce contention
+        let table: Vec<Arc<Mutex<Vec<u8>>>> = (0..lenght)
+            .map(|_| Arc::new(Mutex::new(input.to_vec())))
+            .collect();
+        let table = Arc::new(table);
+
+        // NOTE: could possibly use num_cpus::get_physical();
+        // let optimal_threads = num_cpus::get_physical();
+        let optimal_threads = num_cpus::get();
+        rayon::ThreadPoolBuilder::new()
+            .num_threads(optimal_threads) // Set optimal for your CPU
+            .build_global()
+            .unwrap();
+
+        // updating rows in parallel
+        (0..lenght).into_par_iter().for_each(|i| {
+            let mut row = table[i].lock().unwrap();
+            row.rotate_left(i);
+        });
+
+        let mut table: Vec<Vec<u8>> = table
+            .iter()
+            .map(|row| row.lock().unwrap().clone())
+            .collect();
+
+        table.sort();
+
+        let index = table.iter().position(|row| row == input).unwrap();
+
+        let transformed: Vec<u8> = table.iter().map(|row| row[lenght - 1]).collect();
+
+        (index, transformed)
+    }
+
     pub fn decode(index: usize, input: &[u8]) -> Vec<u8> {
         let lenght = input.len();
 
@@ -74,6 +118,8 @@ pub mod BurrowsWheeler {
 #[cfg(test)]
 mod tests {
 
+    use std::time::Instant;
+
     use super::*;
 
     #[test]
@@ -83,6 +129,40 @@ mod tests {
 
         let result: Vec<u8> = "NNBAAA".bytes().collect();
         assert_eq!((3, result), (index, transformed));
+    }
+
+    #[test]
+    fn burrows_wheeler_encode_sync() {
+        let text: Vec<u8> = "BANANA".bytes().collect();
+        let (index, transformed) = BurrowsWheeler::encode_par(&text);
+
+        let result: Vec<u8> = "NNBAAA".bytes().collect();
+        assert_eq!((3, result), (index, transformed));
+    }
+
+    #[test]
+    fn bwt_encode_benchmark() {
+        // comment to test
+        return;
+
+        #[allow(unreachable_code)]
+        let mut text: Vec<u8> = "BANANA".bytes().collect();
+
+        // DEBUG
+        // let mut bloat: Vec<u8> = "BANANA".bytes().collect();
+        // for _ in 0..1000 {
+        //     text.extend_from_slice(&bloat);
+        // }
+
+        let start = Instant::now();
+        let _ = BurrowsWheeler::encode(&text);
+        let duration_seq = start.elapsed();
+        println!("Time elapsed for sequential encoding: {:?}", duration_seq);
+
+        let start = Instant::now();
+        let _ = BurrowsWheeler::encode_par(&text);
+        let duration_seq = start.elapsed();
+        println!("Time elapsed for parallel: {:?}", duration_seq);
     }
 
     #[test]
